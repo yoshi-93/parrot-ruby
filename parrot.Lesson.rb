7fd0a985c46cd531c2fe57041d36e4088f48f65d
Mario Lopez <ez0212sp@icloud.com>
2020-12-10 18:36:07 -0500

    commit tree 9f43fa4878a2d9b2b716396492c839a907407a45

---
+++ parrot.Lesson.rb
@@ -0,0 +1,49 @@
+
+#3.times { print "Hello!" }
+# > Hello!Hello!Hello!
+
+#3.times { puts "Hello!" }
+# > Hello!
+# > Hello!
+# > Hello!
+
+#def restaurant
+#    restaurant_name = "Guy's American Kitchen & Bar"
+#    cuisine = "american"
+#    motto = "Welcome to Flavor Town!"
+#  end 
+#restaurant
+
+#def print_and_return_name
+#    puts "Guy Fieri"
+#    "Guy Fieri"
+#  end 
+#  print_and_return_name
+
+#def broken_print_and_return_name
+#   "Guy Fieri"
+#  puts "Guy Fieri"
+#end 
+#broken_print_and_return_name
+
+#def stylish_chef
+#    best_hairstyle = "Guy Fieri"
+#    return "Martha Stewart"
+#    "Guy Fieri"
+#  end 
+#stylish_chef
+
+
+def stylish_chef
+    best_hairstyle = "Guy Fieri"
+    return "Martha Stewart"
+    "Guy Fieri"
+  end 
+stylish_chef
+
+
+ 
+
+
+
+
--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,21 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+
+def parrot(phrase = "Squawk!")
+
+  #outpust a string to the console
+  # puts "Squawk!"
+   puts phrase
+  
+  # return "Squawk!"    
+  phrase
+end
+
+
+# outputs the return value of the method parrot
+# puts parrot("Madrid")
+# parrot("Pretty bird!")
+ 
+  
+parrot
